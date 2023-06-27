28f357566b7e5c704e568779bbbbe8e5eb077689
jrexiti <jay.rexiti@gmail.com>
2023-06-27 11:45:52 -0400

    went over list

---
+++ src/Section07/L19_List.java
@@ -0,0 +1,25 @@
+package Section07;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class L19_List {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/jrexiti/Desktop/Resources/drivers_v1/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.get("https://www.automationtesting.co.uk");
+        List<WebElement> elements = driver.findElements(By.cssSelector(".redIcons"));
+
+        
+    }
+
+}
