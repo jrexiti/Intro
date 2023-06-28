6ba1725e4f596d31412da61a7c21744c2446ed2e
jrexiti <jay.rexiti@gmail.com>
2023-06-28 18:36:27 -0400

    more on handling dynamic dropdowns

--- src/AdvancedSelenium/DynamicDropdowns.java
+++ src/AdvancedSelenium/DynamicDropdowns.java
@@ -1,7 +1,13 @@
 package AdvancedSelenium;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class DynamicDropdowns {
 
@@ -12,5 +18,26 @@ public class DynamicDropdowns {
 
         driver.manage().window().maximize();
         driver.get("https://www.easyjet.com/en");
+        new WebDriverWait(driver, 5)
+                .until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.id("ensCloseBanner")));
+        driver.findElement(By.id("ensCloseBanner")).click();
+
+        driver.findElement(By.cssSelector("input[name='origin']")).clear();
+        driver.findElement(By.cssSelector("input[name='origin']")).sendKeys("london");
+
+        List<WebElement> origins = new WebDriverWait(driver, 5)
+                .until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("#ui-id-1 li>a>span")));
+        System.out.println(origins.get(0).getText());
+        System.out.println("----------");
+
+        for (WebElement origin : origins) {
+            System.out.println(origin.getText());
+
+            if (origin.getText().contains("Luton")) {
+                origin.click();
+            }
+
+        }
+
     }
 }
