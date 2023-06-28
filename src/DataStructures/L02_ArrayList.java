b2679f1f14e901eda041f0f22d124123248e6f19
jrexiti <jay.rexiti@gmail.com>
2023-06-28 16:10:55 -0400

    more on Arraylist

---
+++ src/DataStructures/L02_ArrayList.java
@@ -0,0 +1,30 @@
+package DataStructures;
+
+import java.util.ArrayList;
+
+public class L02_ArrayList {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> animal = new ArrayList<String>();
+
+        animal.add("Cat");
+        animal.add("Dog");
+        animal.add("Hamster");
+        animal.add("Rabbit");
+
+        System.out.println("Index of Dog: " + animal.indexOf("Dog") + "\n" + "Value at index [1]: " + animal.get(1));
+
+        for (int i = 0; i < animal.size(); i++) {
+
+            System.out.println(animal.get(i));
+
+        }
+        System.out.println("----------");
+        System.out.println(animal.get(0));
+
+        animal.remove(0);
+        System.out.println(animal.get(0));
+
+    }
+}
